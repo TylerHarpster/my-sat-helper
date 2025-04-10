d9027c72a964d7c1abed92b8b665dce2801fccd5
Tyler Harpster <tylerharpster@mevsd.net>
2025-04-09 21:25:40 -0400

    Added streaks and formatting

--- main.py
+++ main.py
@@ -2,6 +2,18 @@ import ijson
 
 import random
 
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+
 # with open("questions.json", "r") as f:
 #     # Stream items one by one from the top-level array
 #     for question in ijson.items(f, "math.item"):
@@ -9,47 +21,66 @@ import random
 #         print(f"Domain: {question['domain']}")
 #         print(f"Question: {question['question']['question']}\n")
 
-total=0
+qset="english"
 
-with open("questions.json","r",encoding="utf-8") as f:
 
-    for question in ijson.items(f,"math.item"):
-        total+=1
+for i in range(2):
+    total=0
 
-loop=True
+    with open("questions.json","r",encoding="utf-8") as f:
+
+        for question in ijson.items(f,qset+".item"):
+            total+=1
+
+    loop=True
+    print(total)
+    qset="math"
+
+streak=0
 
 def random_question(category):
 
-    selected_q=random.randint(0,total)
+    global streak
+
+    selected_q=random.randint(0,0)
     current_q=0
 
     with open("questions.json","r",encoding="utf-8") as f:
 
         for question in ijson.items(f,category+".item"):
             if(current_q==selected_q):
-                print(f"\n\n\nQuestion {selected_q}: {question['question']['question']}\n")
-                print(f"A: {question['question']['choices']['A']}")
-                print(f"B: {question['question']['choices']['B']}")
-                print(f"C: {question['question']['choices']['C']}")
-                print(f"D: {question['question']['choices']['D']}")
+                print()
+
+                print(f"\nQuestion {selected_q}: {question['question']['question']}")
+                if streak>=3:
+                    print(f"{color.YELLOW}{color.BOLD}You have a {color.RED}{streak}{color.YELLOW} answer streak!{color.END}")
+                print(f"\n{color.RED}A: {question['question']['choices']['A']}")
+                print(f"{color.BLUE}B: {question['question']['choices']['B']}")
+                print(f"{color.YELLOW}C: {question['question']['choices']['C']}")
+                print(f"{color.GREEN}D: {question['question']['choices']['D']}{color.END}")
 
                 user_answer=input("Your answer:")
                 
                 if(user_answer=="quit"): 
-                    loop=False
                     quit()
 
-                correct="Incorrect."
+                correct=False
+                correct_str=color.RED+color.BOLD+"Incorrect."+color.END
                 if(user_answer.lower()==question['question']['correct_answer'].lower()):
-                    correct="Correct!"
-                    if(random.randint(0,100)==15): correct="Good boy ;)"
+                    correct=True
+                    correct_str=color.GREEN+color.BOLD+"Correct!"+color.END
+                    streak+=1
+                    
                 
-                print(f"{correct} {question['question']['explanation']}")
-                print(f"The correct answer was {question['question']['correct_answer']}")
+                print(f"{correct_str} {question['question']['explanation']}")
+                if not correct: print(f"{color.YELLOW} The correct answer was {color.BOLD}{question['question']['correct_answer']}{color.END}")
+
+                if not correct:
+                    streak=0
 
             current_q+=1
 
 
 
 while loop:
-    random_question("math")
+    random_question(qset)
